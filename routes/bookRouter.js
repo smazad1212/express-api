569d90cf069b16586c27cdfa6287a379d728d711
smazad1212 <smazad1212@gmail.com>
2019-09-26 13:40:17 +0600

    controller added

--- routes/bookRouter.js
+++ routes/bookRouter.js
@@ -1,27 +1,13 @@
 const express = require('express')
+const booksController = require('../controllers/booksController')
 
 function routes(Book) {
   const bookRouter = express.Router()
+  const controller = booksController(Book)
 
   bookRouter.route('/books')
-    .post((req, res) => {
-      const book = new Book(req.body)
-
-      book.save()
-      return res.status(201).json(book)
-    })
-    .get((req, res) => {
-      const query = {}
-      if (req.query.genre) {
-        query.genre = req.query.genre
-      }
-      Book.find(query, (err, books) => {
-        if (err) {
-          return res.send(err)
-        }
-        return res.json(books)
-      })
-    })
+    .post(controller.post)
+    .get(controller.get)
   bookRouter.use('/books/:bookId', (req, res, next) => {
     Book.findById(req.params.bookId, (err, book) => {
       if (err) {
@@ -32,7 +18,7 @@ function routes(Book) {
         return next()
       }
       return res.sendStatus(404)
-    })
+    }) 
   })
   bookRouter.route('/books/:bookId')
     .get((req, res) => res.json(req.book))
